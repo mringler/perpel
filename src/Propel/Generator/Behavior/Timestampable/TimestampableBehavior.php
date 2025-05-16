023f8dd257265d57701ad61a4458fbd21a9f2fac
mringler <m@mringler.net>
2025-05-16 11:53:00 +0200

    build ColumnCodeProducers

--- src/Propel/Generator/Behavior/Timestampable/TimestampableBehavior.php
+++ src/Propel/Generator/Behavior/Timestampable/TimestampableBehavior.php
@@ -107,7 +107,7 @@ class TimestampableBehavior extends Behavior
             $updateColumn = $this->getTable()->getColumn($this->getParameter('update_column'));
 
             $dateTimeClass = $builder instanceof ObjectBuilder
-                ? $builder->getDateTimeClass($updateColumn)
+                ? $builder->resolveColumnDateTimeClass($updateColumn)
                 : DateTime::class;
 
             $valueSource = strtoupper($updateColumn->getType()) === 'INTEGER'
@@ -138,7 +138,7 @@ $mtime = PropelDateTime::formatMicrotime(microtime(true));';
             $createColumn = $this->getTable()->getColumn($this->getParameter('create_column'));
 
             $dateTimeClass = $builder instanceof ObjectBuilder
-                ? $builder->getDateTimeClass($createColumn)
+                ? $builder->resolveColumnDateTimeClass($createColumn)
                 : DateTime::class;
 
             $script .= "
@@ -157,7 +157,7 @@ if (!\$this->isColumnModified(" . $this->getColumnConstant('create_column', $bui
             $updateColumn = $this->getTable()->getColumn($this->getParameter('update_column'));
 
             $dateTimeClass = $builder instanceof ObjectBuilder
-                ? $builder->getDateTimeClass($updateColumn)
+                ? $builder->resolveColumnDateTimeClass($updateColumn)
                 : DateTime::class;
 
             $script .= "
